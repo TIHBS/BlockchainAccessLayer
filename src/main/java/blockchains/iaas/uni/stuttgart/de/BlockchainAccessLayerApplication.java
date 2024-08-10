e645a7f58703d79614bc13f29eba57812674d67f
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2024-08-10 15:51:54 +0200

    Initial switch to spring boot with Spring Web

---
+++ src/main/java/blockchains/iaas/uni/stuttgart/de/BlockchainAccessLayerApplication.java
@@ -0,0 +1,13 @@
+package blockchains.iaas.uni.stuttgart.de;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainAccessLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainAccessLayerApplication.class, args);
+	}
+
+}
